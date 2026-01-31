caa56b376a062ab098c8b5423d7dde66b35e2f49
Sudharshan A <asudharshan38@gmail.com>
2026-01-31 19:20:52 +0530

    Add files via upload

---
+++ assessment11/problem04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void find_numbers() 
+{
+    int num, tens, units;
+    for (num = 10; num <= 99; num++) 
+    {
+        if (num % 2 != 0) {  
+            tens = num / 10;
+            units = num % 10;
+            if (tens + units == 7) {
+                printf("%d\n", num);
+            }
+        }
+    }
+}
+int main() {
+    printf("odd number :\n");
+    find_numbers();
+    return 0;
+}
