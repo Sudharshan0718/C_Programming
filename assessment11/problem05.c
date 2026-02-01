5ae9d64b9ebd17d0caed21664a75c0a16cd002e0
Sudharshan A <asudharshan38@gmail.com>
2026-02-01 20:16:50 +0530

    Add files via upload

---
+++ assessment11/problem05.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void memorycopy(int* src, int* dst, int size) {
+    int i;
+    for (i = 0; i < size * sizeof(int); i++) {
+        dst[i] = src[i];
+    }
+}
+
+int main() {
+    int source[] = {10, 20, 30, 40, 50};
+    int dest[5];
+    int size = 5;  // Number of integers
+
+    printf("Source: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", source[i]);
+    }
+    printf("\n");
+
+    memorycopy(source, dest, size);
+
+    printf("Copied to dest: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", dest[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ assessment11/problem06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void stringcopy(char* src, char* dst) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';  // Add null terminator
+}
+
+int main() {
+    char source[] = "Hello World";
+    char dest[20];  // Enough space for copy
+
+    printf("Original source: %s\n", source);
+
+    stringcopy(source, dest);
+
+    printf("Copied string: %s\n", dest);
+
+    return 0;
+}
